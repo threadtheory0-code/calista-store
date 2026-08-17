476017438ff7baacc439bc5899540f7b09e40395
threadtheory0-code <threadtheory0@gmail.com>
2026-08-17 17:02:17 +0500

    Add files via upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "pk.calista.merchant"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.5.0"
+        versionCode = 8
+        versionName = "1.5.1"
     }
 
     buildTypes {
